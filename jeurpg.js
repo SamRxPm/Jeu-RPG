f80ef1dc5aa2b54c49d87127cac465b3bbee4aa8
Sam <118293930+SamRxPm@users.noreply.github.com>
2022-12-10 19:31:13 +0100

    Jeu-rpg

---
+++ jeurpg.js
@@ -0,0 +1,31 @@
+var pvHero1Html = document.getElementById("PV1");
+var pvHero1 = "200";
+
+function degats1(){
+    pvHero1 -= 20;
+    pvHero1Html.innerHTML = pvHero1;
+}
+
+var pvHero2Html = document.getElementById("PV2");
+var pvHero2 = "300";
+
+function degats2(){
+    pvHero2 -= 10;
+    pvHero2Html.innerHTML = pvHero2;
+}
+
+var pvHero2Html = document.getElementById("PV2");
+var pvHero2 = "200";
+
+function degats4(){
+    pvHero2 -= 10;
+    pvHero2Html.innerHTML = pvHero2;
+}
+
+var pvHero3Html = document.getElementById("PV3");
+var pvHero3 = "200";
+
+function degats3(){
+    pvHero3 -= 20;
+    pvHero3Html.innerHTML = pvHero3;
+}
